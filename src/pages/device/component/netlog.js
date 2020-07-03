77880e9d0d7ee1336b5c086fd78cf2066be43704
Rake Yang <fenglaijun@gmail.com>
2020-07-03 16:48:04 +0800

    优化网络日志：太长时间显示...防止看不全

--- src/pages/device/component/netlog.js
+++ src/pages/device/component/netlog.js
@@ -26,11 +26,11 @@ export default class NetLogPage extends React.Component {
             getContainer={false}
             style={{ position: 'absolute' }}
         >
-            <Descriptions title="" column={2} size='small' layout="vertical" bordered>
-                <Descriptions.Item label={<b>请求</b>} className={styles.logtop}><ReactJson src={data.requestfields} name={'Headers'}></ReactJson></Descriptions.Item>
-                <Descriptions.Item label={<b>响应</b>} className={styles.logtop}><ReactJson src={data.responsefields} name={'Headers'}></ReactJson></Descriptions.Item>
-                <Descriptions.Item label="Body" className={styles.logtop}><ReactJson src={data.requestbody || {}} name={false}></ReactJson></Descriptions.Item>
-                <Descriptions.Item label="Body" className={styles.logtop}><ReactJson src={data.responsebody || {}} name={false}></ReactJson></Descriptions.Item>
+            <Descriptions title="" column={2} size='small' layout="vertical" bordered  column={{ xxl: 2, xl: 2, lg: 2, md: 1, sm: 1, xs: 1 }}>
+                <Descriptions.Item label={<b>请求</b>} className={styles.logtop}><ReactJson src={data.requestfields} name={'Headers'} collapseStringsAfterLength={50}></ReactJson></Descriptions.Item>
+                <Descriptions.Item label={<b>响应</b>} className={styles.logtop}><ReactJson src={data.responsefields} collapseStringsAfterLength={50} name={'Headers'}></ReactJson></Descriptions.Item>
+                <Descriptions.Item label="Body" className={styles.logtop}><ReactJson src={data.requestbody || {}} name={false} collapseStringsAfterLength={50}></ReactJson></Descriptions.Item>
+                <Descriptions.Item label="Body" className={styles.logtop}><ReactJson src={data.responsebody || {}} name={false} collapseStringsAfterLength={50}></ReactJson></Descriptions.Item>
             </Descriptions>
         </Drawer>)
     }

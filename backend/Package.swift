18f7c0b1402fd81fb94e6c5276d329d2fa901a2a
Rake Yang <fenglaijun@gmail.com>
2021-06-15 11:15:42 +0800

    升级swiftjson

--- backend/Package.swift
+++ backend/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .package(name: "PerfectWebSockets", url: "https://github.com/PerfectlySoft/Perfect-WebSockets.git", from: "3.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.0"),
         .package(url: "https://github.com/onevcat/Rainbow", .upToNextMajor(from: "4.0.0")),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
 	],
 	targets: [
         .target(

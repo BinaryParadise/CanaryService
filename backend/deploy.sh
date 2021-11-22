a5c96fc3db57e534981ad09e10ce994b756864c7
Rake Yang <fenglaijun@gmail.com>
2021-11-22 11:08:32 +0800

    优化部署脚本

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,9 +1,18 @@
+A="$1"
+dest="--linux"
+name="canary"
+if [ "$A" = "$dest" ]
+then
+  name="x86_64-unknown-linux-gnu"
+else
+  echo "x86_64-apple-macosx"
+fi
 git reset --hard
 git pull
 rm -f Package.resolved
 swift build
 if [ -f ".build/release/CanaryBackend" ];then
-    cp -r .build/release/CanaryBackend release/canary
+    cp -r .build/release/CanaryBackend release/$name
   else  
-    cp -r .build/debug/CanaryBackend release/canary
+    cp -r .build/debug/CanaryBackend release/$name
 fi
